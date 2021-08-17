6a9e38fadd55b7f11ea7e4d81e9019deb8084797
tashasun <natalia.limbakh@gmail.com>
2021-08-17 18:46:32 +0300

    feat: solved task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+exports.min = function mini(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let result = array.reduce((minimum, current) => {
+        if (minimum > current) minimum = current;
+        return minimum;
+    }, Infinity);
+    return result;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function maxi(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let result = array.reduce((maximum, current) => {
+        if (maximum < current) maximum = current;
+        return maximum;
+    }, -Infinity);
+    return result;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let result = array.reduce((sum, current) => sum + current, 0);
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    return result / array.length;
+};
+
+// console.log(avge([1,5,1,5,2,4]));
+// // console.log(avg([1, 2, 8, 9, 10, 11, 5]));
